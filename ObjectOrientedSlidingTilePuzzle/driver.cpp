e5ec49ce3e9a38b81385fe418aadd57a5a045ec0
shibiwhy <ian.welch19@icloud.com>
2022-10-26 21:10:48 -0400

    add some includes, consts, and seed rng in main()

--- ObjectOrientedSlidingTilePuzzle/driver.cpp
+++ ObjectOrientedSlidingTilePuzzle/driver.cpp
@@ -1,8 +1,36 @@
 #include "specification.h"
 #include <iostream>
+#include <iomanip>
+#include <time.h>
+#include <Windows.h>
+
+
+#define NUM_ROWS		3		
+#define NUM_COLS		3		
+#define PIVOT         NUM_ROWS * NUM_COLS // highest number on board to be changed to *
+#define NUM_SCRAMBLE    1000000 // number of times to scramble board
+
+#define PIVOT_SYMBOL	"*"		// used to show the pivot location when drawing the board
+
+// direction codes (part of the slideTile() interface)
+#define SLIDE_UP		1		// pass to slideTile() to trigger UP movement
+#define SLIDE_DOWN		2		// pass to slideTile() to trigger DOWN movement
+#define SLIDE_LEFT		3		// pass to slideTile() to trigger LEFT movement
+#define SLIDE_RIGHT		4		// pass to slideTile() to trigger RIGHT movement
+#define BRUTE_FORCE     999     // pass to inputLoop() to solve with RNG
+
+#define UNSET			-1		// used to arbitrarily indicate an undetermined state in a constuct
+
+#define COLOR_DEFAULT	7
+#define COLOR_RED		12
+#define COLOR_GREEN		10
 
 
 int main() {
+
+	// seed pseudo rng
+	srand(time(NULL));
+
 	SlidingPuzzle puzzle0;
 
 	return 0;
